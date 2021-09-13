6e265463043aa2ffa36e567004ccb5d11e574849
Pomysłowy Radomir <pgarbacz84@gmail.com>
2021-09-13 12:18:38 +0200

    Added Basic FOV calculation from anglular FOV

--- lens.py
+++ lens.py
@@ -1,5 +1,16 @@
-
-
 import math
 
+def FOVfromAngleD(_angFOV, _WD):
+    hang = float(_angFOV)/2
+    fWD = float(_WD)
+    hFOV = abs(math.tan(hang)*fWD)
+    FOV = 2*hFOV
+    return FOV
 print("This is basic lens calculator")
+
+angFOV = input("Write angular field of view [degrees]: ")
+WD = input("Write working distance [mm]: ")
+
+print("Field of view of your camera setup is " + str(FOVfromAngleD(angFOV,WD)) + "mm")
+
+
